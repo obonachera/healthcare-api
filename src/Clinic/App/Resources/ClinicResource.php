d9a2b45fb034f28055d4eba234e273a74f9104c2
obonachera <obonachera@example.com>
2026-04-22 16:51:08 -0300

    feat: modify clinic app entities to add doctor relationship

--- src/Clinic/App/Resources/ClinicResource.php
+++ src/Clinic/App/Resources/ClinicResource.php
@@ -21,6 +21,7 @@ class ClinicResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'location' => $this->address,
+            'doctors_count' => $this->doctors_count ?? 0,
         ];
     }
 }
