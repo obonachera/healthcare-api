e3b5e0b7bb9577628abe795aa4e1f80baab71d59
obonachera <obonachera@example.com>
2026-04-22 16:47:37 -0300

    feat: add actions to list, store and update a doctor

---
+++ src/Doctor/Domain/Actions/StoreDoctorAction.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Doctor\Domain\Actions;
+
+use Lightit\Doctor\Domain\DataTransferObjects\DoctorDto;
+use Lightit\Doctor\Domain\Models\Doctor;
+
+class StoreDoctorAction
+{
+    public function execute(DoctorDto $doctorDto): Doctor
+    {
+        $doctor = new Doctor();
+        $doctor->name = $doctorDto->name;
+        $doctor->saveOrFail();
+
+        $doctor->clinics()->sync($doctorDto->clinics);
+
+        return $doctor->load(['clinics' => fn ($q) => $q->withCount('doctors')]);
+    }
+}
