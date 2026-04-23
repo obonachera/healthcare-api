84e06fc94f41b89dd18a38742b9dd9a6efc49caa
obonachera <obonachera@example.com>
2026-04-23 17:00:11 -0300

    fix: remove use of incompatible pgsql method

--- database/migrations/2026_04_22_121234_create_doctors_table.php
+++ database/migrations/2026_04_22_121234_create_doctors_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->useCurrentOnUpdate();
+            $table->timestamp('updated_at')->useCurrent();
             $table->softDeletes();
         });
     }
